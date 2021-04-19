9e81ac43f737f46f55a957927184ad5534ee27d4
Tommy Veen <75741894+tommyveen@users.noreply.github.com>
2021-04-19 22:31:53 +0200

    Add files via upload

---
+++ JavaScript_006/index.js
@@ -0,0 +1,20 @@
+
+
+function makePizza() {
+console.log('Make dough');
+console.log('Put topping on the pizza')
+console.log('Put the pizza in the oven')
+}
+
+makePizza()
+
+
+function makeSushi() {
+    console.log('Cut fish')
+    console.log('Wrap it in Seaweed')
+    console.log('Cut the sushi in pieces')
+}
+
+makeSushi
+
+
